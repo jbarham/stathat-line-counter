3115aede797a98a2db121937efcef5fdc4818377
John Barham <jbarham@gmail.com>
2012-07-05 20:05:29 +1000

    Add main.go and update README

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
 stathat-line-counter
 ====================
 
-Go program that reads stdin and logs lines read to StatHat
+__stathat-line-counter__ is a [Go](http://golang.org) program that reads stdin
+and periodically logs lines read to StatHat.
+
+It's useful for tracking statistics like web server hits, DNS queries etc.
+
+Installation
+------------
+
+	go get github.com/jbarham/stathat-line-counter
+	go install github.com/jbarham/stathat-line-counter
+
+Example Usage
+-------------
+
+To log the number of Nginx hits every minute:
+
+	tail -F /var/log/nginx/access.log | stathat-line-counter -stat "web hits" -ezkey yourname@example.com
+
+(Note the use of the -F vs. -f flag to re-open the log file when it's rotated.)
+
+About
+-----
+
+__stathat-line-counter__ was written by John Barham (jbarham@gmail.com).
+
+It assumes you have an account with [StatHat](http://www.stathat.com), which is
+a great statistics tracking service, and coincidentally is [largely written
+in Go](http://blog.golang.org/2011/12/building-stathat-with-go.html).
---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"github.com/stathat/stathatgo"
+	"os"
+	"time"
+)
+
+var (
+	stat    = flag.String("stat", "", "statistic name")
+	ezkey   = flag.String("ezkey", "", "EZ key")
+	period  = flag.Int("period", 60, "seconds between stat updates")
+	verbose = flag.Bool("v", false, "verbose logging")
+)
+
+func main() {
+	flag.Parse()
+
+	if *stat == "" {
+		fmt.Fprintln(os.Stderr, "stat must be specified")
+		os.Exit(2)
+	}
+
+	if *ezkey == "" {
+		fmt.Fprintln(os.Stderr, "ezkey must be specified")
+		os.Exit(2)
+	}
+
+	lineCounter := readlines()
+	tick := time.Tick(time.Duration(*period) * time.Second)
+	n := 0
+	for {
+		select {
+		case <-lineCounter:
+			n++
+		case <-tick:
+			stathat.PostEZCount(*stat, *ezkey, n)
+			if *verbose {
+				fmt.Printf("%s: %d lines\n", time.Now().Format("2006-01-02 15:04:05"), n)
+			}
+			n = 0
+		}
+	}
+}
+
+func readlines() chan bool {
+	c := make(chan bool)
+	go func() {
+		r := bufio.NewReader(os.Stdin)
+		for {
+			_, isPrefix, err := r.ReadLine()
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "ReadLine error:", err)
+				// Overkill?  Assumes process will be automatically restarted on exit.
+				os.Exit(1)
+			}
+			if !isPrefix {
+				c <- true
+			}
+		}
+	}()
+	return c
+}
